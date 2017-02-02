1d8a9c0d3d2d5988d8ef658f18914b88a10b5514
James Hayhurst <James.A.Hayhurst@gmail.com>
2017-02-02 02:30:52 -0500

    finished up tests for ch4

--- ch4/comma_code_test.py
+++ ch4/comma_code_test.py
@@ -2,16 +2,19 @@
 '''
 Automate the Boring Stuff with Python
 generic testing for chapter 4 projects
+
+This test covers comma_code
+
 Jack Hayhurst
 '''
 
 from io import StringIO
 import unittest
 from unittest.mock import patch
-import projects
+import comma_code
 
 
-class TestProjects(unittest.TestCase):
+class TestCommaCode(unittest.TestCase):
     '''tests the collatz.py script'''
 
     def test_comma_code(self):
@@ -24,33 +27,8 @@ class TestProjects(unittest.TestCase):
                   'apples, bananas, tofu, True, and cats'))
         for testdata in tests:
             testinput, testoutput = (list(testdata[:-1]), str(testdata[-1]))
-            self.assertEqual(projects.comma_code(testinput), testoutput)
+            self.assertEqual(comma_code.comma_code(testinput), testoutput)
 
-    def test_picture_grid(self):
-        '''single test to verify picture transfer'''
-
-        grid = [['.', '.', '.', '.', '.', '.'],
-                ['.', 'O', 'O', '.', '.', '.'],
-                ['O', 'O', 'O', 'O', '.', '.'],
-                ['O', 'O', 'O', 'O', 'O', '.'],
-                ['.', 'O', 'O', 'O', 'O', 'O'],
-                ['O', 'O', 'O', 'O', 'O', '.'],
-                ['O', 'O', 'O', 'O', '.', '.'],
-                ['.', 'O', 'O', '.', '.', '.'],
-                ['.', '.', '.', '.', '.', '.']]
-
-        output = """
-..OO.OO..
-.OOOOOOO.
-.OOOOOOO.
-..OOOOO..
-...OOO...
-....O....
-""".strip()
-
-        with patch('sys.stdout', new=StringIO()) as fakeOutput:
-            projects.picture_grid(grid)
-            self.assertEqual(fakeOutput.getvalue().strip(), output)
 
 if __name__ == "__main__":
     unittest.main()
--- ch4/picture_grid_test.py
+++ ch4/picture_grid_test.py
@@ -8,23 +8,11 @@ Jack Hayhurst
 from io import StringIO
 import unittest
 from unittest.mock import patch
-import projects
+import picture_grid
 
 
-class TestProjects(unittest.TestCase):
+class TestPictureGrid(unittest.TestCase):
     '''tests the collatz.py script'''
-
-    def test_comma_code(self):
-        '''table driven test to verify list combination'''
-        tests = (('apples', 'bananas', 'tofu', 'cats',
-                  'apples, bananas, tofu, and cats'),
-                 ('apples', 'bananas', 5, 'cats',
-                  'apples, bananas, 5, and cats'),
-                 ('apples', 'bananas', 'tofu', True, 'cats',
-                  'apples, bananas, tofu, True, and cats'))
-        for testdata in tests:
-            testinput, testoutput = (list(testdata[:-1]), str(testdata[-1]))
-            self.assertEqual(projects.comma_code(testinput), testoutput)
 
     def test_picture_grid(self):
         '''single test to verify picture transfer'''
@@ -49,7 +37,7 @@ class TestProjects(unittest.TestCase):
 """.strip()
 
         with patch('sys.stdout', new=StringIO()) as fakeOutput:
-            projects.picture_grid(grid)
+            picture_grid.picture_grid(grid)
             self.assertEqual(fakeOutput.getvalue().strip(), output)
 
 if __name__ == "__main__":
